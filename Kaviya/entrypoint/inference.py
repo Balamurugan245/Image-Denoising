0c6d0f67d7b98de181efa8752ebccdca3e3b1464
kaviya <kaviyarajasekaran247@gmail.com>
2026-01-05 17:30:47 +0530

    Update inference.py

--- Kaviya/entrypoint/inference.py
+++ Kaviya/entrypoint/inference.py
@@ -1,7 +1,7 @@
 import torch
 from torch.utils.data import DataLoader
 
-from src.model import UNet
+from src.unet_model import UNet
 from src.dataset import NoisyOnlyDataset
 from src.inference_pipeline import inference
 from src.utils import load_config, get_device
