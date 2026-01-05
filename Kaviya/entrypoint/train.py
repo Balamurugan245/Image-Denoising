c4925490a74e6635d5c373ac19753c5a94093a10
kaviya <kaviyarajasekaran247@gmail.com>
2026-01-05 17:28:55 +0530

    Update train.py

--- Kaviya/entrypoint/train.py
+++ Kaviya/entrypoint/train.py
@@ -1,6 +1,3 @@
-import sys, os
-sys.path.append(os.path.abspath(".."))
-
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -9,24 +6,23 @@ from torch.utils.data import DataLoader
 from src.model import UNet
 from src.dataset import DrawingDenoiseDataset
 from src.loss_function import SSIMLoss
-from src.train_pipeline import train_one_epoch
+from src.train_pipeline import train
+from src.utils import load_config, get_device
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
+cfg = load_config("config/config.yaml")
+device = get_device(cfg)
 
 dataset = DrawingDenoiseDataset(
-    noisy_dir="data/01-raw/noisy",
-    clean_dir="data/01-raw/clean"
+    cfg["paths"]["noisy_train"],
+    cfg["paths"]["clean_train"],
+    cfg["training"]["img_size"]
 )
 
-loader = DataLoader(dataset, batch_size=4, shuffle=True)
+loader = DataLoader(dataset, batch_size=cfg["training"]["batch_size"], shuffle=True)
 
 model = UNet().to(device)
-optimizer = optim.Adam(model.parameters(), lr=1e-4)
+optimizer = optim.Adam(model.parameters(), lr=cfg["training"]["lr"])
 
-loss_fn = lambda p, t: 0.8 * nn.L1Loss()(p, t) + 0.2 * SSIMLoss()(p, t)
+train(model, loader, optimizer, nn.L1Loss(), SSIMLoss(), cfg, device)
 
-for epoch in range(200):
-    loss = train_one_epoch(model, loader, optimizer, loss_fn, device)
-    print(f"Epoch {epoch+1} | Loss: {loss:.4f}")
-
-torch.save(model.state_dict(), "unet_denoiser.pth")
+torch.save(model.state_dict(), cfg["paths"]["save_model"])
