a70f7a90ff7b224ef772789bf05015df52a2f019
kaviya <kaviyarajasekaran247@gmail.com>
2026-01-05 17:04:39 +0530

    Update dataset.py

--- Kaviya/src/dataset.py
+++ Kaviya/src/dataset.py
@@ -1,14 +1,13 @@
-import os
-from PIL import Image
 from torch.utils.data import Dataset
+from PIL import Image
 import torchvision.transforms as T
+import os
 
-class DenoiseDataset(Dataset):
-    def __init__(self, root, img_size=512):
-        self.noisy_dir = os.path.join(root, "Noisy")
-        self.clean_dir = os.path.join(root, "Clean")
-
-        self.files = sorted(os.listdir(self.noisy_dir))
+class DrawingDenoiseDataset(Dataset):
+    def __init__(self, noisy_dir, clean_dir, img_size=224):
+        self.files = sorted(os.listdir(noisy_dir))
+        self.noisy_dir = noisy_dir
+        self.clean_dir = clean_dir
 
         self.transform = T.Compose([
             T.Resize((img_size, img_size)),
@@ -19,12 +18,7 @@ class DenoiseDataset(Dataset):
         return len(self.files)
 
     def __getitem__(self, idx):
-        x = Image.open(
-            os.path.join(self.noisy_dir, self.files[idx])
-        ).convert("RGB")
-
-        y = Image.open(
-            os.path.join(self.clean_dir, self.files[idx])
-        ).convert("RGB")
-
-        return self.transform(x), self.transform(y)
+        name = self.files[idx]
+        noisy = Image.open(os.path.join(self.noisy_dir, name)).convert("RGB")
+        clean = Image.open(os.path.join(self.clean_dir, name)).convert("RGB")
+        return self.transform(noisy), self.transform(clean)
