78858554ab8cea10ca03c77b9a880f77c5535953
kaviya <kaviyarajasekaran247@gmail.com>
2026-01-05 17:13:53 +0530

    Update utils.py

--- Kaviya/src/utils.py
+++ Kaviya/src/utils.py
@@ -1,1 +1,14 @@
+import yaml
+import torch
+import random
+import numpy as np
 
+def load_config(path="config/local.yaml"):
+    with open(path, "r") as f:
+        return yaml.safe_load(f)
+
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
