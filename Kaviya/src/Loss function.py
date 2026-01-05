80334cb00bf73e61f5e02dc05339dedf95ec4a9c
kaviya <kaviyarajasekaran247@gmail.com>
2026-01-05 17:03:37 +0530

    Update Loss function.py

--- Kaviya/src/Loss function.py
+++ Kaviya/src/Loss function.py
@@ -1,1 +1,23 @@
+import torch.nn as nn
+import torch.nn.functional as F
 
+class SSIMLoss(nn.Module):
+    def __init__(self, window_size=11):
+        super().__init__()
+        self.window_size = window_size
+
+    def forward(self, x, y):
+        C1 = 0.01 ** 2
+        C2 = 0.03 ** 2
+
+        mu_x = F.avg_pool2d(x, self.window_size, 1, self.window_size // 2)
+        mu_y = F.avg_pool2d(y, self.window_size, 1, self.window_size // 2)
+
+        sigma_x = F.avg_pool2d(x*x, self.window_size, 1, self.window_size // 2) - mu_x**2
+        sigma_y = F.avg_pool2d(y*y, self.window_size, 1, self.window_size // 2) - mu_y**2
+        sigma_xy = F.avg_pool2d(x*y, self.window_size, 1, self.window_size // 2) - mu_x*mu_y
+
+        ssim = ((2*mu_x*mu_y + C1)*(2*sigma_xy + C2)) / \
+               ((mu_x**2 + mu_y**2 + C1)*(sigma_x + sigma_y + C2))
+
+        return 1 - ssim.mean()
