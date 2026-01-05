8cbad694490216e8988cb6890abdfd47be8798e7
kaviya <kaviyarajasekaran247@gmail.com>
2026-01-05 17:29:22 +0530

    Update inference.py

--- Kaviya/entrypoint/inference.py
+++ Kaviya/entrypoint/inference.py
@@ -1,26 +1,18 @@
-import sys, os
-sys.path.append(os.path.abspath(".."))
-
 import torch
-import torchvision.utils as vutils
 from torch.utils.data import DataLoader
 
 from src.model import UNet
-from src.noisy_dataset import NoisyOnlyDataset
+from src.dataset import NoisyOnlyDataset
+from src.inference_pipeline import inference
+from src.utils import load_config, get_device
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
+cfg = load_config("config/config.yaml")
+device = get_device(cfg)
 
 model = UNet().to(device)
-model.load_state_dict(torch.load("unet_denoiser.pth", map_location=device))
-model.eval()
+model.load_state_dict(torch.load(cfg["paths"]["save_model"], map_location=device))
 
-dataset = NoisyOnlyDataset("data/01-raw/noisy")
+dataset = NoisyOnlyDataset(cfg["paths"]["test_dir"])
 loader = DataLoader(dataset, batch_size=1)
 
-os.makedirs("data/04-predictions", exist_ok=True)
-
-with torch.no_grad():
-    for noisy, name in loader:
-        noisy = noisy.to(device)
-        pred = model(noisy)
-        vutils.save_image(pred, f"data/04-predictions/{name[0]}")
+inference(model, loader, cfg["paths"]["output_dir"], device)
