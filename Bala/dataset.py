d37943dbc30f3abff352499f7e0792b249d37660
Bala Murugan <144324318+Balamurugan245@users.noreply.github.com>
2026-01-05 16:31:41 +0530

    Update dataset.py

--- Bala/dataset.py
+++ Bala/dataset.py
@@ -1,3 +1,4 @@
+# dataset.py
 import os
 from PIL import Image
 from torch.utils.data import Dataset
@@ -8,23 +9,35 @@ class DenoiseDataset(Dataset):
         self.noisy_dir = os.path.join(root, "Noisy")
         self.clean_dir = os.path.join(root, "Clean")
 
-        self.files = sorted(os.listdir(self.noisy_dir))
+        noisy_files = os.listdir(self.noisy_dir)
+        clean_files = os.listdir(self.clean_dir)
+
+        noisy_map = {os.path.splitext(f)[0]: f for f in noisy_files}
+        clean_map = {os.path.splitext(f)[0]: f for f in clean_files}
+
+        self.keys = sorted(noisy_map.keys() & clean_map.keys())
+        self.noisy_map = noisy_map
+        self.clean_map = clean_map
 
         self.transform = T.Compose([
             T.Resize((img_size, img_size)),
             T.ToTensor()
         ])
 
+        print("Total matched image pairs:", len(self.keys))
+
     def __len__(self):
-        return len(self.files)
+        return len(self.keys)
 
     def __getitem__(self, idx):
-        x = Image.open(
-            os.path.join(self.noisy_dir, self.files[idx])
+        key = self.keys[idx]
+
+        noisy = Image.open(
+            os.path.join(self.noisy_dir, self.noisy_map[key])
         ).convert("RGB")
 
-        y = Image.open(
-            os.path.join(self.clean_dir, self.files[idx])
+        clean = Image.open(
+            os.path.join(self.clean_dir, self.clean_map[key])
         ).convert("RGB")
 
-        return self.transform(x), self.transform(y)
+        return self.transform(noisy), self.transform(clean)
