c10422135bcc9f7b8bc68a33c5f2a08d635e5fe4
kaviya <kaviyarajasekaran247@gmail.com>
2026-01-05 18:10:31 +0530

    Update utils.py

--- Kaviya/src/utils.py
+++ Kaviya/src/utils.py
@@ -1,9 +1,36 @@
-import yaml
+import matplotlib.pyplot as plt
 import torch
 
-def load_config(path):
-    with open(path) as f:
-        return yaml.safe_load(f)
+def show_samples(model, dataset, device, num=5):
+    model.eval()
+    idxs = torch.randperm(len(dataset))[:num]
 
-def get_device(cfg):
-    return "cuda" if torch.cuda.is_available() and cfg["device"] == "auto" else "cpu"
+    plt.figure(figsize=(12, 3 * num))
+    for i, idx in enumerate(idxs):
+        noisy, clean = dataset[idx]
+        noisy_b = noisy.unsqueeze(0).to(device)
+
+        with torch.no_grad():
+            pred = model(noisy_b)
+
+        noisy = noisy.permute(1,2,0).cpu()
+        clean = clean.permute(1,2,0)
+        pred = pred.squeeze().permute(1,2,0).cpu()
+
+        plt.subplot(num, 3, i*3+1)
+        plt.imshow(noisy)
+        plt.title("Noisy")
+        plt.axis("off")
+
+        plt.subplot(num, 3, i*3+2)
+        plt.imshow(clean)
+        plt.title("Clean GT")
+        plt.axis("off")
+
+        plt.subplot(num, 3, i*3+3)
+        plt.imshow(pred)
+        plt.title("UNet Pred")
+        plt.axis("off")
+
+    plt.tight_layout()
+    plt.show()
