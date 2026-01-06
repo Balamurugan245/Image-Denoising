0cdb3247ec638c58259015911591ee34faac4fb8
kaviya <kaviyarajasekaran247@gmail.com>
2026-01-06 21:27:47 +0530

    Update main.py

--- Kaviya/Unet/main.py
+++ Kaviya/Unet/main.py
@@ -1,30 +1,4 @@
-import torch
-from config import Config
-from dataset import get_dataloaders
-from Unet import UNet
 from train_model import Trainer
 
-DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-
-def main():
-    train_loader, val_loader = get_dataloaders()
-
-    model = UNet().to(DEVICE)
-
-    optimizer = torch.optim.Adam(model.parameters(), lr=Config.lr)
-    loss_fn = Config.loss_fn
-
-    trainer = Trainer(
-        model=model,
-        train_loader=train_loader,
-        val_loader=val_loader,
-        optimizer=optimizer,
-        loss_fn=loss_fn,
-        device=DEVICE
-    )
-
-    trainer.train()
-
 if __name__ == "__main__":
-    main()
-
+    train()
