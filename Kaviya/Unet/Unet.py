ade95024dc51634d2836ba778aa0582e71852439
kaviya <kaviyarajasekaran247@gmail.com>
2026-01-06 19:34:22 +0530

    Update Unet.py

---
+++ Kaviya/Unet/Unet.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+from unet_parts import DoubleConv
+
+
+class UNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.d1 = DoubleConv(1, 64)
+        self.d2 = DoubleConv(64, 128)
+        self.d3 = DoubleConv(128, 256)
+
+        self.pool = nn.MaxPool2d(2)
+        self.up = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True)
+
+        self.u1 = DoubleConv(256 + 128, 128)
+        self.u2 = DoubleConv(128 + 64, 64)
+
+        self.out = nn.Conv2d(64, 1, kernel_size=1)
+
+    def forward(self, x):
+        c1 = self.d1(x)
+        c2 = self.d2(self.pool(c1))
+        c3 = self.d3(self.pool(c2))
+
+        x = self.up(c3)
+        x = self.u1(torch.cat([x, c2], dim=1))
+
+        x = self.up(x)
+        x = self.u2(torch.cat([x, c1], dim=1))
+
+        return self.out(x)
