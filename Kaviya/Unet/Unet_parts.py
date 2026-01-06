9035432dfbc9d7f7192ccf5d5a2868c729b43664
kaviya <kaviyarajasekaran247@gmail.com>
2026-01-06 21:32:16 +0530

    Update Unet_parts.py

--- Kaviya/Unet/Unet_parts.py
+++ Kaviya/Unet/Unet_parts.py
@@ -1,5 +1,7 @@
-import torch as t
+import torch
 import torch.nn as nn
+import torch.nn.functional as F
+
 
 class DoubleConv(nn.Module):
     def __init__(self, in_ch, out_ch):
@@ -12,8 +14,10 @@ class DoubleConv(nn.Module):
             nn.BatchNorm2d(out_ch),
             nn.ReLU(inplace=True),
         )
+
     def forward(self, x):
         return self.conv(x)
+
 
 class Down(nn.Module):
     def __init__(self, in_ch, out_ch):
@@ -22,26 +26,37 @@ class Down(nn.Module):
             nn.MaxPool2d(2),
             DoubleConv(in_ch, out_ch)
         )
+
     def forward(self, x):
         return self.down(x)
+
 
 class Up(nn.Module):
     def __init__(self, in_ch, out_ch):
         super().__init__()
-        self.up = nn.ConvTranspose2d(in_ch, in_ch//2, 2, stride=2)
+        self.up = nn.ConvTranspose2d(in_ch, in_ch // 2, 2, stride=2)
         self.conv = DoubleConv(in_ch, out_ch)
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
-        diffY = x2.size()[2] - x1.size()[2]
-        diffX = x2.size()[3] - x1.size()[3]
-        x1 = nn.functional.pad(x1, [diffX//2, diffX-diffX//2, diffY//2, diffY-diffY//2])
+
+        diffY = x2.size(2) - x1.size(2)
+        diffX = x2.size(3) - x1.size(3)
+
+        x1 = F.pad(
+            x1,
+            [diffX // 2, diffX - diffX // 2,
+             diffY // 2, diffY - diffY // 2]
+        )
+
         x = torch.cat([x2, x1], dim=1)
         return self.conv(x)
+
 
 class OutConv(nn.Module):
     def __init__(self, in_ch, out_ch):
         super().__init__()
         self.conv = nn.Conv2d(in_ch, out_ch, 1)
+
     def forward(self, x):
         return self.conv(x)
