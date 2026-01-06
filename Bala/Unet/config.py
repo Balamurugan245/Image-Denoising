520b136999f96792433a40f489250afeb4e5f7e1
kaviya <kaviyarajasekaran247@gmail.com>
2026-01-06 20:08:01 +0530

    Create config.py

---
+++ Bala/Unet/config.py
@@ -0,0 +1,18 @@
+class Config:
+    # data
+    image_size = 512
+    batch_size = 2
+    val_ratio = 0.2
+
+    # training
+    epochs = 40
+    lr = 1e-4
+    weight_decay = 1e-5
+
+    # loss weights
+    ssim_weight = 0.5
+
+    # paths (Kaggle)
+    data_root = "/kaggle/input/input-data/Dataset-1k/New_Data100"
+    checkpoint_dir = "checkpoints"
+    prediction_dir = "predictions"
