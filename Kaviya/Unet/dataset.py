b8feef6f7b91646c4b35f3e16e1cfb219bccabb1
kaviya <kaviyarajasekaran247@gmail.com>
2026-01-12 13:57:11 +0530

    Update dataset.py

--- Kaviya/Unet/dataset.py
+++ Kaviya/Unet/dataset.py
@@ -6,28 +6,24 @@ from sklearn.model_selection import train_test_split
 
 
 class DenoisingDataset(Dataset):
-    def __init__(self, noisy_dir, clean_dir, split="train", val_ratio=0.2):
+    def __init__(self, noisy_dir, clean_dir, split="train", val_ratio=0.2, transform=None):
         self.noisy_dir = noisy_dir
         self.clean_dir = clean_dir
+        self.transform = transform if transform else transforms.ToTensor()
 
         noisy_images = sorted(os.listdir(noisy_dir))
         clean_images = sorted(os.listdir(clean_dir))
 
-        assert len(noisy_images) == len(clean_images), \
-            "Noisy and Clean folders must have same number of images"
+        assert len(noisy_images) == len(clean_images)
 
         indices = list(range(len(noisy_images)))
         train_idx, val_idx = train_test_split(
-            indices,
-            test_size=val_ratio,
-            random_state=42,
-            shuffle=True
+            indices, test_size=val_ratio, random_state=42, shuffle=True
         )
 
         self.indices = train_idx if split == "train" else val_idx
         self.noisy_images = noisy_images
         self.clean_images = clean_images
-        self.transform = transform if transform else transforms.ToTensor()
 
     def __len__(self):
         return len(self.indices)
