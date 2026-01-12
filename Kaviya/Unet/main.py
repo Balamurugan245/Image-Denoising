872da80e62edbe9b79eda59d6f51b57e6e3ad2e6
kaviya <kaviyarajasekaran247@gmail.com>
2026-01-12 13:05:04 +0530

    Update main.py

--- Kaviya/Unet/main.py
+++ Kaviya/Unet/main.py
@@ -3,7 +3,7 @@ from torch.utils.data import DataLoader
 from config import Config
 from dataset import DenoisingDataset
 from Unet import UNet
-from train_model import train
+from train_model import Trainer
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 print("Device:", DEVICE)
@@ -13,6 +13,7 @@ train_ds = DenoisingDataset(
     f"{Config.data_root}/Clean",
     split="train"
 )
+
 val_ds = DenoisingDataset(
     f"{Config.data_root}/Noisy",
     f"{Config.data_root}/Clean",
@@ -23,15 +24,18 @@ train_loader = DataLoader(train_ds, batch_size=Config.batch_size, shuffle=True)
 val_loader = DataLoader(val_ds, batch_size=1)
 
 model = UNet().to(DEVICE)
+
 optimizer = torch.optim.Adam(
     model.parameters(),
     lr=Config.lr,
     weight_decay=Config.weight_decay
 )
 
-trainer = train()
-trainer.start(model, Config.epochs, Config.checkpoint_dir)
-
-
-
-
+trainer = Trainer(device=DEVICE)
+trainer.start(
+    model=model,
+    train_loader=train_loader,
+    val_loader=val_loader,
+    optimizer=optimizer,
+    epochs=Config.epochs
+)
