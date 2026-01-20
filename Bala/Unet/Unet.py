fbd686d2b42e5f42ea7d27a86f83bc5349f458f6
Bala Murugan <144324318+Balamurugan245@users.noreply.github.com>
2026-01-20 17:53:47 +0530

    Update Unet.py

--- Bala/Unet/Unet.py
+++ Bala/Unet/Unet.py
@@ -27,27 +27,35 @@ class Up(nn.Module):
 class UNetCBAM(nn.Module):
     def __init__(self):
         super().__init__()
-        self.d1 = Down(3, 64)
+        self.d1 = Down(1, 64)     
         self.d2 = Down(64, 128)
         self.d3 = Down(128, 256)
         self.d4 = Down(256, 512)
+
         self.bottleneck = DoubleConv(512, 1024)
+
         self.u1 = Up(1024, 512)
         self.u2 = Up(512, 256)
         self.u3 = Up(256, 128)
         self.u4 = Up(128, 64)
-        self.out = nn.Conv2d(64, 3, 1)
+
+        self.out = nn.Conv2d(64, 1, 1)  
 
     def forward(self, x):
         c1, p1 = self.d1(x)
         c2, p2 = self.d2(p1)
         c3, p3 = self.d3(p2)
         c4, p4 = self.d4(p3)
+
         b = self.bottleneck(p4)
+
         x = self.u1(b, c4)
         x = self.u2(x, c3)
         x = self.u3(x, c2)
         x = self.u4(x, c1)
+
         return torch.sigmoid(self.out(x))
+
+
 
 
