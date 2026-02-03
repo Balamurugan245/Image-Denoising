89b8eb972f58439b03cd7df01b24922ae1e0a837
Bala Murugan <144324318+Balamurugan245@users.noreply.github.com>
2026-02-03 12:47:44 +0530

    Update Unet_parts.py

--- Bala/Unet/Unet_parts.py
+++ Bala/Unet/Unet_parts.py
@@ -1,56 +1,52 @@
-import torch
+import torch as t
 import torch.nn as nn
 
-class ChannelAttention(nn.Module):
-    def __init__(self, channels, reduction=16):
-        super().__init__()
-        self.avg = nn.AdaptiveAvgPool2d(1)
-        self.max = nn.AdaptiveMaxPool2d(1)
-        self.fc = nn.Sequential(
-            nn.Conv2d(channels, channels // reduction, 1, bias=False),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(channels // reduction, channels, 1, bias=False)
-        )
-        self.sigmoid = nn.Sigmoid()
-
-    def forward(self, x):
-        return self.sigmoid(self.fc(self.avg(x)) + self.fc(self.max(x))) * x
-
-
-class SpatialAttention(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.conv = nn.Conv2d(2, 1, 7, padding=3, bias=False)
-        self.sigmoid = nn.Sigmoid()
-
-    def forward(self, x):
-        avg = torch.mean(x, dim=1, keepdim=True)
-        max,_ = torch.max(x, dim=1, keepdim=True)
-        return self.sigmoid(self.conv(torch.cat([avg, max], 1))) * x
-
-
-class CBAM(nn.Module):
-    def __init__(self, channels):
-        super().__init__()
-        self.ca = ChannelAttention(channels)
-        self.sa = SpatialAttention()
-
-    def forward(self, x):
-        return self.sa(self.ca(x))
-
-
+from Attention_Mechanism import ResidualCBAMBlock
+device = 'cuda' if t.cuda.is_available() else 'cpu'
 class DoubleConv(nn.Module):
-    def __init__(self, in_c, out_c):
+    def __init__(self, in_channel, out_channels):
         super().__init__()
-        self.net = nn.Sequential(
-            nn.Conv2d(in_c, out_c, 3, padding=1),
-            nn.BatchNorm2d(out_c),
+        self.conv_op = nn.Sequential(
+            nn.Conv2d(in_channel, out_channels, kernel_size=(3,3), padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(out_c, out_c, 3, padding=1),
-            nn.BatchNorm2d(out_c),
+            nn.Conv2d(out_channels, out_channels, kernel_size=(3,3), padding=1),
             nn.ReLU(inplace=True),
-            CBAM(out_c)
         )
 
     def forward(self, x):
-        return self.net(x)
+        return self.conv_op(x)
+    
+class Downsample(nn.Module):
+    def __init__(self, in_channel, out_channel, attention_residual= False):
+        super().__init__()
+        self.conv = DoubleConv(in_channel, out_channel)
+        self.pool = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
+        self.attention_residual = attention_residual
+        
+        if attention_residual:
+            self.res = ResidualCBAMBlock(out_channel,out_channel)
+
+    def forward(self, x):
+        conv = self.conv(x)
+        if self.attention_residual:
+            conv = self.res(conv)
+        pool = self.pool(conv)
+        return conv, pool
+    
+class UpSample(nn.Module):
+    def __init__(self, in_channel, out_channel,attention_residual=False):
+        super().__init__()
+        self.up = nn.ConvTranspose2d(in_channel, in_channel//2, kernel_size=(2,2), stride=2)
+        self.conv = DoubleConv(in_channel, out_channel)
+        self.attention_residual = attention_residual
+        
+        if attention_residual:
+            self.res = ResidualCBAMBlock(out_channel,out_channel)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        x = t.cat([x1, x2], 1)
+        x = self.conv(x)
+        if self.attention_residual:
+            x = self.res(x)
+        return x
