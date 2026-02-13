43a23de9bcc77e0b0b6ddece3d87abc8b2315487
Bala Murugan <144324318+Balamurugan245@users.noreply.github.com>
2026-02-13 14:43:49 +0530

    Update dataset.py

--- Bala/Unet/dataset.py
+++ Bala/Unet/dataset.py
@@ -4,8 +4,10 @@ from PIL import Image
 from torch.utils.data import Dataset
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
+from config import Config
 
-IMAGE_SIZE_NEW =384
+IMAGE_SIZE = Config.IMAGE_SIZE
+
 def get_train_transform():
     return A.Compose([
             A.Resize(IMAGE_SIZE_NEW,IMAGE_SIZE_NEW),
@@ -80,6 +82,7 @@ class DenoiseDataset(Dataset):
         clean = clean.float() / 225
 
         return noisy, clean
+
 
 
 
